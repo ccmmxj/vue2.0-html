cb5590eb0d15f3d390290f658a759b89538171ff
CaoMengjiao <mengjiao.cao@teradata.com>
2018-06-14 16:58:01 +0800

    test

--- src/base/addr.js
+++ src/base/addr.js
@@ -1,4 +1,4 @@
-var host = 'http://100.64.29.249:8080/'
+var host = 'http://220.190.177.127:8080/'
 const audio = 'https://ai.baidu.com/aidemo?type=tns2&idx=1&cuid=baidu_speech_demo&cod=2&ctp=1&pdt=1&spd=5&per=0&vol=5&pit=5&tex='
 
 export default { host,
