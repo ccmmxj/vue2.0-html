c9ffe6423de18e6e513523baccbf480039064837
CaoMengjiao <mengjiao.cao@teradata.com>
2018-06-15 18:47:47 +0800

    url

--- src/base/addr.js
+++ src/base/addr.js
@@ -1,5 +1,5 @@
 var host = 'http://220.190.179.149:8001/'
-const audio = 'https://ai.baidu.com/aidemo?type=tns2&idx=1&cuid=baidu_speech_demo&cod=2&ctp=1&pdt=1&spd=5&per=0&vol=5&pit=5&tex='
+const audio = 'https://ai.baidu.com/aidemo?type=tns2&idx=1&cuid=baidu_speech_demo&cod=2&ctp=1&pdt=1&spd=3&per=0&vol=5&pit=5&tex='
 
 export default { host,
   audioUrl (text, type) {
