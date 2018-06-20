96732beb613b5c04eee5d5da1ec69048cc111e72
CaoMengjiao <mengjiao.cao@teradata.com>
2018-06-20 14:41:56 +0800

    ip更新

--- src/base/addr.js
+++ src/base/addr.js
@@ -1,4 +1,4 @@
-var host = 'http://220.190.179.149:8001/'
+var host = 'http://220.190.178.165:8001/'
 const audio = 'https://ai.baidu.com/aidemo?type=tns2&idx=1&cuid=baidu_speech_demo&cod=2&ctp=1&pdt=1&spd=2&per=0&vol=5&pit=5&tex='
 
 export default { host,
